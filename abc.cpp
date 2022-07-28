127b9ff7e853784a9e5eac9633b7c92ed3d4a4ee
123 <alsttabblt@outlook.com>
2022-07-28 21:06:01 +0800

    123

--- abc.cpp
+++ abc.cpp
@@ -3,14 +3,21 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+const int ArSize = 8;
+int sum_arr(int arr[], int n); // arr=array name,n=size
 int main(void)
 {
-    printf("HelloWorld.\n");
-    printf("---------------------------------\n");
-    printf("This is a code test:\n");
-    printf("Test1\n");
-    
-    printf("---------------------------------\n");
-    std::cin.get();
+    int cookies[ArSize] = {1, 2, 4, 8, 16, 32, 64, 128};
+    int sum = sum_arr(cookies, ArSize);
+    std::cout << "Total cookies eaten: " << sum << "\n";
     return 0;
+}
+int sum_arr(int arr[], int n)
+{
+    int total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        total = total + arr[i];
+    }
+    return total;
 }
