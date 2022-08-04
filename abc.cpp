e291fda8e87f9bfa534ff9080d005be41e744c41
123 <alsttabblt@outlook.com>
2022-08-04 17:38:33 +0800

    123

--- abc.cpp
+++ abc.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cstdio>
 const int ArSize = 8;
-int sum_arr(int arr[], int n); // arr=array name,n=size
-int fill_array(double ar[],int limit);//函数
+int sum_arr(int arr[], int n);             // arr=array name,n=size
+int fill_array(double ar[], int limit);    //函数
+void show_array(const double ar[], int n); //函数
 int main(void)
 {
     int cookies[ArSize] = {1, 2, 4, 8, 16, 32, 64, 128};
@@ -19,27 +20,37 @@ int sum_arr(int arr[], int n)
     }
     return total;
 }
-int fill_array(double ar[],int limit){//函数
-    double temp;//声明一个double变量
-    int i;//声明一个int变量
-    for (i = 0; i < limit; i++)//for循环，i赋值为0，如果i<limit开始执行循环，条件不满足将会停止
+int fill_array(double ar[], int limit)
+{                               //函数
+    double temp;                //声明一个double变量
+    int i;                      //声明一个int变量
+    for (i = 0; i < limit; i++) // for循环，i赋值为0，如果i<limit开始执行循环，条件不满足将会停止
     {
-        std::cout<<"Enter value #"<<(i+1)<<": ";//打印输出
-        std::cin>>temp;//输入temp变量，接收输入内容
-        if (!std::cin)//bad input如果cin发生了错误，将会执行if语句内容
+        std::cout << "Enter value #" << (i + 1) << ": "; //打印输出
+        std::cin >> temp;                                //输入temp变量，接收输入内容
+        if (!std::cin)                                   // bad input如果cin发生了错误，将会执行if语句内容
         {
-            std::cin.clear();//std::cin.clear让错误标识改回为0
-            while (std::cin.get()!= '\n')//输入内容只要不是换行符，将执行while循环
+            std::cin.clear();              // std::cin.clear让错误标识改回为0
+            while (std::cin.get() != '\n') //输入内容只要不是换行符，将执行while循环
             {
-                continue;//结束本次循环，进行下一次循环，而不终止整个循环的执行。
+                continue; //结束本次循环，进行下一次循环，而不终止整个循环的执行。
             }
-            std::cout<<"Bad input; input process terminated.\n";//打印输出
+            std::cout << "Bad input; input process terminated.\n"; //打印输出
             break;
-        }else if (temp<0)//如果temp<0将执行这一个
-        {
-            break;//跳出分支
         }
-        ar[i]=temp;//赋值，将temp的值赋给ar[i]
+        else if (temp < 0) //如果temp<0将执行这一个
+        {
+            break; //跳出分支
+        }
+        ar[i] = temp; //赋值，将temp的值赋给ar[i]
     }
-    return i;//函数最后返回i
+    return i; //函数最后返回i
+}
+void show_array(const double ar[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "Property #" << (i + 1) << ": $";
+        std::cout << ar[i] << std::endl;
+    }
 }
